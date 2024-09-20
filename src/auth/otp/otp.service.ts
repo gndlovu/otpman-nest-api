59e4320797cc8c0b8bda63a3f292de6d2729d4c0
Gladwell Ndlovu <gladwell_n@live.com>
2024-09-20 18:22:45 +0000

    Updated OTP checks

--- src/auth/otp/otp.service.ts
+++ src/auth/otp/otp.service.ts
@@ -10,30 +10,13 @@ export class OtpService {
   constructor(private readonly dbService: DatabaseService, private readonly configService: ConfigService) {}
 
   async generateOtp(userId: number): Promise<string> {
-    const now = new Date();
-    const twentyFourHoursAgo = new Date(now.getTime() - 60 * 60 * 24 * 1000);
-     // const expireSec = 120;
-    // const expiresAt = new Date(now.getTime() + (1000 * expireSec));
-
     // 3. A user cannot request more than X OTPs per hour. X is a config/environment variable set to 3 for now,
+    //   we should be able to change this easily.
     if (await this.hasReachedHourlyLimit(userId)) {
-      // throw new UnprocessableEntityException(
-      //   `Maximum of ${this.configService.get<number>('MAX_OTP_REQ_PER_HR')} OTP requests per hour has been reached.`
-      // );
+      throw new UnprocessableEntityException(
+        `Maximum of ${this.configService.get<number>('MAX_OTP_REQ_PER_HR')} OTP requests per hour has been reached.`
+      );
     }
-
-    // 
-    // let otp = await this.dbService.otp.findFirst({
-    //   where: {
-    //     AND: [
-    //       { userId },
-    //       { createdAt: { gt: twentyFourHoursAgo } }, // 2. A user cannot receive the same OTP number if it's within a 24 hour period.
-    //       { isExpired: false },
-    //       { isUsed: false } // 8. An OTP cannot be used more than once.
-    //     ]
-    //   },
-    //   orderBy: { createdAt: 'desc' } // 4. Only the latest OTP can be valid at any given time.
-    // });
 
     // 4. Only the latest OTP can be valid at any given time.
     let otp = await this.dbService.otp.findFirst({
@@ -41,37 +24,27 @@ export class OtpService {
       orderBy: { createdAt: 'desc' }
     });
     
-    console.log('latestOtp: ', otp);
+    const now = new Date();
 
-    if (!otp || otp.expiresAt < now) {
+    if (!otp || otp.expiresAt.getTime() < now.getTime()) {
       // First time generation / token expired
       otp = await this.generateNewOtp(userId);
-      console.log('newOtp no checks: ', otp);
+      console.log('First time generation / token expired: ', otp);
     } else {
-      // Do some checks before generating
+      const twentyFourHoursAgo = new Date();
+      twentyFourHoursAgo.setHours(twentyFourHoursAgo.getHours() - 24);
 
-    /*-1. The OTP should be 6 digits long and it should be possible for it to start with 0.
-      - 2. A user cannot receive the same OTP number if it's within a 24 hour period. If that happens, a new one
-      should be generated and the user should not be made aware that the same one was randomly
-      generated.
-      - 3. A user cannot request more than X OTPs per hour. X is a config/environment variable set to 3 for now,
-      we should be able to change this easily.
-      - 4. Only the latest OTP can be valid at any given time.
-      - 5. An OTP expires after X seconds. X is a config/environment variable set to 30 seconds for now.
-      - 6. If a user requests the OTP to be resent within X minutes, it will resend the original OTP (and update the
-      expiry) instead of generating a new one. X is a config variable set to 5 for now.
-      - 7. An OTP cannot be resent more than X times. X is a config variable set to 3 for now.
-      - 8. An OTP cannot be used more than once.*/
-
-      if (twentyFourHoursAgo > otp.createdAt) {
+      if (twentyFourHoursAgo.getTime() > otp.createdAt.getTime()) {
         otp = await this.generateNewOtp(userId);
+        console.log('A user cannot receive the same OTP number if it\'s within a 24 hour period.', otp);
       } else {
         // 6. If a user requests the OTP to be resent within X minutes, it will resend the original OTP (and update the expiry) instead of generating a new one. X is a config variable set to 5 for now.
         const resendMinInterval = new Date();
-        resendMinInterval.setMinutes(resendMinInterval.getMinutes() + this.configService.get<number>('OTP_RESEND_MIN_INTERVAL'));
-        if (resendMinInterval > otp.createdAt) {
+        resendMinInterval.setMinutes(resendMinInterval.getMinutes() + +this.configService.get<number>('OTP_RESEND_MIN_INTERVAL'));
+
+        if (resendMinInterval.getTime() > otp.createdAt.getTime()) {
           const expiresAt = new Date();
-          expiresAt.setSeconds(expiresAt.getSeconds() + this.configService.get<number>('OTP_EXPIRE_SEC'));
+          expiresAt.setSeconds(expiresAt.getSeconds() + +this.configService.get<number>('OTP_EXPIRE_SEC'));
           otp.expiresAt = expiresAt;
 
           await this.dbService.otp.update({
@@ -80,11 +53,14 @@ export class OtpService {
             },
             data: otp,
           });
+
+          console.log('If a user requests the OTP to be resent within X minutes, it will resend the original OTP', otp);
         }
 
         // 7. An OTP cannot be resent more than X times. X is a config variable set to 3 for now.
-        if (otp.resentCount >= this.configService.get<number>('OTP_MAX_RESEND')) {
+        if (otp.resentCount >= +this.configService.get<number>('OTP_MAX_RESEND')) {
           otp = await this.generateNewOtp(userId);
+          console.log('7. An OTP cannot be resent more than X times. X is a config variable set to 3 for now.', otp);
         } else {
           otp.resentCount++;
           await this.dbService.otp.update({
@@ -93,6 +69,8 @@ export class OtpService {
             },
             data: otp,
           });
+
+          console.log('resentCount++.', otp);
         }
       }
     }
@@ -120,7 +98,7 @@ export class OtpService {
     
     // 5. An OTP expires after X seconds. X is a config/environment variable set to 30 seconds for now.
     const expiresAt = new Date();
-    expiresAt.setSeconds(expiresAt.getSeconds() + this.configService.get<number>('OTP_EXPIRE_SEC'));
+    expiresAt.setSeconds(expiresAt.getSeconds() + +this.configService.get<number>('OTP_EXPIRE_SEC'));
 
     const payload: Prisma.OtpUncheckedCreateInput = { pin, userId, expiresAt };
 
